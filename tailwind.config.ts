72193711207026d29fc988f6540d30cc379be25f
Osatohanmwen Solomon <solomonosato18@gmail.com>
2023-10-12 21:56:45 +0100

    Transfer some config from the .js file to the .ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: {
@@ -21,10 +29,26 @@ const config: Config = {
       },
       backgroundImage: {
         gradient:
-          "linear-gradient(to top, hsl(240, 73%, 65%), hsl(273, 75%, 66%))",
+          "linear-gradient(to top, hsl(240, 73%, 65%) 10%, hsl(273, 75%, 66%) 50%)",
+        mobile: "url('/images/bg-pattern-mobile.svg')",
+        desktop: "url('/images/bg-pattern-desktop.svg')",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
