5b3532ecf5c511680fc6892ed5b04bc92786065b
Osatohanmwen Solomon <solomonosato18@gmail.com>
2023-10-12 15:04:16 +0100

    Setup colors and backgrounds

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          blue: "hsl(238 29% 16%)",
+          red: "hsl(14 88% 65%)",
+        },
+        neutral: {
+          blue: "hsl(240 6% 50%)",
+          vblue: "hsl(237 12% 33%)",
+          divider: "hsl(240 5% 91%)",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        gradient:
+          "linear-gradient(to top, hsl(240, 73%, 65%), hsl(273, 75%, 66%))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
